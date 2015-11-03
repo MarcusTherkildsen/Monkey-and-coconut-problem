799ed22dbf5ffa9d243c451088edca52c9f49d9c
Marcus Therkildsen <marcustherkildsen@gmail.com>
2015-11-03 20:09:59 +0100

    Solution in Java

--- monkey-java/src/com/example/monkeyjava/MonkeyJava.java
+++ monkey-java/src/com/example/monkeyjava/MonkeyJava.java
@@ -9,37 +9,31 @@ public class MonkeyJava {
     public static void main(String[] args) {
 
         // Creating some start variables
-        float num_sailors = 5;
-        int search_to = 3500;
-        float perc = (1 - 1/num_sailors);
+        int sailors = 5;
+        int monkeys = 1;
+        int coconuts_tot = 0;
 
-        // For a certain amount of coconuts to begin with
-        for (int i=0; i<search_to; i++) {
-            // Since taking the modulo can result in non-integer fractions we declare the number of coconuts to float
-            float num_coc = i;
+        // Go through the number of coconuts
+        while (0 == 0){
+            coconuts_tot += 1;
+            int coconuts = coconuts_tot;
 
             // Go through the number of sailors
-            for (int j=0; j<num_sailors;j++){
+            for (int j=0; j<sailors;j++){
 
-                // Removing 1 coconut (for the monkey) and the amount that the certain sailor takes for himself
-                num_coc = (num_coc-1)*perc;
-
+                // One for each monkey
+                coconuts -= monkeys;
+                if (coconuts % sailors != 0){
+                    break;
+                }
+                coconuts -= coconuts/sailors;
             }
 
-            /*
-            Now each sailor has his own pile.
-            The monkey has 5 and there's a pile left.
-            Checking if there is an amount divisible by num_sailors left in the pile
-            and if the number of remaining is positive (of course)
-             */
-
-            if ((num_coc % num_sailors == 0) & (num_coc >= 0)) {
-                /* If the remaining (positive) number of coconuts is divisible between the sailors, then we found
-                the solution.
-                */
-                System.out.println("Solution: " + i + " coconuts to begin with");
+            if (coconuts % sailors == 0){
+                break;
             }
         }
 
+        System.out.println("Solution: " + coconuts_tot + " coconuts to begin with");
     }
 }
