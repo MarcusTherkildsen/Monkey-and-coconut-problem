0fbd1d48f99ace264195f7d0ab1fd00f0c733343
Marcus Therkildsen <marcustherkildsen@gmail.com>
2015-11-06 21:34:31 +0100

    Solution in C#
    Indents sorted out

--- monkey.cs
+++ monkey.cs
@@ -9,16 +9,16 @@ public class MonkeyCSharp {
     // This is the main, equivalent to if __name__ == '__main__': in python
     public static void Main(String[] args) {
         
-        // Creating some start variables
+    // Creating some start variables
     int sailors = 5;
-	int monkeys = 1;
+    int monkeys = 1;
     int j;
-	int coconuts_tot = 0;
+    int coconuts_tot = 0;
     int coconuts;
 	
     // Go through the number of coconuts
     while (0 == 0){
-		coconuts_tot += 1;
+	coconuts_tot += 1;
         coconuts = coconuts_tot;
         
         // Go through the number of sailors
@@ -26,15 +26,15 @@ public class MonkeyCSharp {
 
             // One for each monkey
             coconuts -= monkeys;      
-			if (coconuts % sailors != 0){
-				break;
+            if (coconuts % sailors != 0){
+                break;
 			}			
-			coconuts -= coconuts/sailors;	
-		}
+            coconuts -= coconuts/sailors;	
+            }
 		
-		if (coconuts % sailors == 0){
-			break;        
-		} 
+        if (coconuts % sailors == 0){
+	    break;        
+	    } 
     }
     Console.Write("Solution: " + coconuts_tot + " coconuts to begin with");		
     }
